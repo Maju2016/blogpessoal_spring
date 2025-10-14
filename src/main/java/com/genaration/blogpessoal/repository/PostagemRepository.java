808be79b5d012a5f7f9b45978f603d348cde1cce
Maria <mjuliamedeiros.2016@gmail.com>
2025-10-14 11:46:36 -0300

    :sparkles: Finalização do CRUD do Recurso Postagem

--- src/main/java/com/genaration/blogpessoal/repository/PostagemRepository.java
+++ src/main/java/com/genaration/blogpessoal/repository/PostagemRepository.java
@@ -1,9 +1,14 @@
 package com.genaration.blogpessoal.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.genaration.blogpessoal.model.Postagem;
 
 public interface PostagemRepository extends JpaRepository<Postagem, Long>{
+	
+	public List<Postagem> findAllByTituloContainingIgnoreCase(String titulo);
+	//SELECT * FROM tb_postagens WHERE titulo LIKE "%?"
 	
 }
